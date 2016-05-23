e8decb70358c8cea75e5e21529dcb70b736c411f
Pavan Praveen Kulkarni <kulkarnipavanp@gmail.com>
2016-05-22 23:41:26 -0700

    ok

---
+++ Trees/src/com/pavan/Main/BinaryTree.java
@@ -0,0 +1,22 @@
+package com.pavan.Main;
+
+import java.util.*;
+import java.awt.*;
+
+
+
+public class BinaryTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated
+		
+		
+		System.out.println("Hello");
+		
+		System.out.println("abc");
+		
+		
+
+	}
+
+}
